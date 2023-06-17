29bbed06d9b3595826eb9b3200d55caaaa376b2f
Rafael <rafaelukf@gmail.com>
2023-06-17 14:03:15 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -20,7 +19,9 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+}
+body{
+  background-color: #77B9B4;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="home">
+    <div class="desc">
+      <div class="logos">
+        <div class="img">
+          <img src="../assets/logo.svg">
+        </div>
+        <div class="social">
+          <img src="../assets/f.svg" alt="facebook">
+          <img src="../assets/w.svg" alt="whatsapp">
+          <img src="../assets/i.svg" alt="ig">
+          <img src="../assets/m.svg" alt="email">
+        </div>
+      </div>
+      <div class="text">
+        <h1>SERVICIO PODOLÓGICO ESPECIALIZADOS Y TERAPIAS REHABILITADORAS</h1>
+        <p>¡Cuida tus pies, regala bienestar! Con nuestros servicios podológicos especializados y terapias rehabilitadoras, caminarás hacia una vida más saludable.</p>
+      </div>
+    </div>
+    <div class="landing__img">
+      <img src="../assets/podiatry-landing.jpg">
+    </div>
+  </div>
+  <div class="services">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.social img{
+  margin-left: 10px;
+}
+.logos{
+  display: flex;
+  justify-content: space-around;
+}
+.home{
+  display: flex;
+  padding: 0 50px 0 50px;
+}
+
+.desc{
+  padding-right: 50px;
+}
+.landing__img{
+  padding: 0 30px 0;
+}
+
+.text h1{
+  @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&family=Roboto:wght@300&display=swap');
+  font-family: 'Roboto', sans-serif;
+  font-size: 40px;
+
+}
+.text p{
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 32px;
+}
+</style>
